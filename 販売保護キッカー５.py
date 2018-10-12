973f8b88dcc5ad15e3c84b564a465cdcf5924d83
どまおー <17107514+Dosugamea@users.noreply.github.com>
2018-10-12 15:07:29 +0900

    Update 販売保護キッカー５.py
    *Still not works

--- 販売保護キッカー５.py
+++ 販売保護キッカー５.py
@@ -84,12 +84,12 @@ class FuncPs(object):
         gr = cl.getGroup(gid)
         #DANGEROUS
         if gr.preventedJoinByTicket == False:
-            Ticket = yukino_1.reissueGroupTicket(gid)
+            Ticket = cl.reissueGroupTicket(gid)
             for i in bots:
                 i.acceptGroupInvitationByTicket(gid, Ticket)
         else:
             groupInfomation.preventedJoinByTicket = False
-            yukino_1.updateGroup(groupInfomation)
+            cl.updateGroup(groupInfomation)
             Ticket = yukino_1.reissueGroupTicket(gid)
             for i in bots:
                 i.acceptGroupInvitationByTicket(gid, Ticket)
@@ -215,7 +215,7 @@ class CmdPs(object):
         
     @tracer.Command()
     def ginfo(self,cl,msg):
-        group = yukino_1.getGroup(msg.to)
+        group = cl.getGroup(msg.to)
         md =  "[グループ名]:\n" + group.name + "\n\n"
         md += "[gid]:\n" + group.id + "\n\n"
         md += "[アイコン画像]:\n" + "http://dl.profile.line-cdn.net/"
@@ -255,202 +255,123 @@ class CmdPs(object):
             if m not in protect['whiteUsers']:
                 protect['whiteUsers'][whiteUserMid] = True
                 added  += cl.getContact(m).displayName + "\n"
-        cl.replyMessage(msg, "%s\nAdded these users to whitelist!"%(added))
+        if added != "":
+            cl.replyMessage(msg, "%s\nAdded these users to whitelist!"%(added))
+        else:
+            cl.replyMessage(msg, "All mentioned users are already in whitelist!")
         
     @tracer.Command(permissions=["Admin","White"])
     def 保護_オン(self,cl,msg):
-        if msg._from in protect['whiteUsers']:
-            if msg.to in protect['normalProtect']:
-                yukino_1.sendMessage(msg.to, "保護機能はすでに有効になっています。")
-            else:
-                protect['normalProtect'][msg.to] = True
-                readData = codecs.open('./normalProtect.json','w','utf-8')
-                json.dump(protect['normalProtect'], readData, sort_keys=True, indent=4, ensure_ascii=False)
-                yukino_1.sendMessage(msg.to, "保護機能を有効にしました。")
-        elif msg._from in admins:
-            if msg.to in protect['normalProtect']:
-                yukino_1.sendMessage(msg.to, "保護機能はすでに有効になっています。")
-            else:
-                protect['normalProtect'][msg.to] = True
-                readData = codecs.open('./normalProtect.json','w','utf-8')
-                json.dump(protect['normalProtect'], readData, sort_keys=True, indent=4, ensure_ascii=False)
-                yukino_1.sendMessage(msg.to, "保護機能を有効にしました。")
+        if msg.to in protect['normalProtect']:
+            cl.replyMessage(msg, "保護機能はすでに有効になっています。")
+        else:
+            protect['normalProtect'][msg.to] = True
+            readData = codecs.open('./normalProtect.json','w','utf-8')
+            json.dump(protect['normalProtect'], readData, sort_keys=True, indent=4, ensure_ascii=False)
+            cl.replyMessage(msg, "保護機能を有効にしました。")
         
     @tracer.Command(permissions=["Admin","White"])
     def 保護_オフ(self,cl,msg):
-        if msg._from in protect['whiteUsers']:
-            if msg.to in protect['normalProtect']:
-                del protect['normalProtect'][msg.to]
-                readData = codecs.open('./normalProtect.json','w','utf-8')
-                json.dump(protect['normalProtect'], readData, sort_keys=True, indent=4, ensure_ascii=False)
-                yukino_1.sendMessage(msg.to, "保護機能を解除しました。")
-            else:
-                yukino_1.sendMessage(msg.to, "保護機能はすでに無効になっています。")
-        elif msg._from in admins:
-            if msg.to in protect['normalProtect']:
-                del protect['normalProtect'][msg.to]
-                readData = codecs.open('./normalProtect.json','w','utf-8')
-                json.dump(protect['normalProtect'], readData, sort_keys=True, indent=4, ensure_ascii=False)
-                yukino_1.sendMessage(msg.to, "保護機能を解除しました。")
-            else:
-                yukino_1.sendMessage(msg.to, "保護機能はすでに無効になっています。")
+        if msg.to in protect['normalProtect']:
+            del protect['normalProtect'][msg.to]
+            readData = codecs.open('./normalProtect.json','w','utf-8')
+            json.dump(protect['normalProtect'], readData, sort_keys=True, indent=4, ensure_ascii=False)
+            cl.replyMessage(msg, "保護機能を解除しました。")
+        else:
+            cl.replyMessage(msg, "保護機能はすでに無効になっています。")
 
     @tracer.Command(permissions=["Admin","White"])
     def 保護_名前_オン(self,cl,msg):
-        if msg._from in protect['whiteUsers']:
-            if msg.to in protect['groupName']:
-                yukino_1.sendMessage(msg.to, "グループ名の保護機能はすでに有効になっています。")
-            else:
-                protect['groupName'][msg.to] = yukino_1.getGroup(msg.to).name
-                f=codecs.open('./groupName.json','w','utf-8')
-                json.dump(protect['groupName'], f, sort_keys=True, indent=4,ensure_ascii=False)
-                yukino_1.sendMessage(msg.to, "グループ名の保護機能を有効にしました。")
-        elif msg._from in admins:
-            if msg.to in protect['groupName']:
-                yukino_1.sendMessage(msg.to, "グループ名の保護機能はすでに有効になっています。")
-            else:
-                protect['groupName'][msg.to] = yukino_1.getGroup(msg.to).name
-                f=codecs.open('./groupName.json','w','utf-8')
-                json.dump(protect['groupName'], f, sort_keys=True, indent=4,ensure_ascii=False)
-                yukino_1.sendMessage(msg.to, "グループ名の保護機能を有効にしました。。")
+        if msg.to in protect['groupName']:
+            cl.replyMessage(msg, "グループ名の保護機能はすでに有効になっています。")
+        else:
+            protect['groupName'][msg.to] = cl.getGroup(msg.to).name
+            f=codecs.open('./groupName.json','w','utf-8')
+            json.dump(protect['groupName'], f, sort_keys=True, indent=4,ensure_ascii=False)
+            cl.replyMessage(msg, "グループ名の保護機能を有効にしました。")
         
     @tracer.Command(permissions=["Admin","White"])
     def 保護_名前_オフ(self,cl,msg):
-        if msg._from in protect['whiteUsers']:
-            if msg.to in protect['groupName']:
-                del protect['groupName'][msg.to]
-                readData = codecs.open('./groupName.json','w','utf-8')
-                json.dump(protect['groupName'], readData, sort_keys=True, indent=4, ensure_ascii=False)
-                yukino_1.sendMessage(msg.to, "グループ名の保護機能を解除しました。")
-            else:
-                yukino_1.sendMessage(msg.to, "グループ名の保護機能はすでに無効になっています。")
-        elif msg._from in admins:
-            if msg.to in protect['groupName']:
-                del protect['groupName'][msg.to]
-                readData = codecs.open('./groupName.json','w','utf-8')
-                json.dump(protect['groupName'], readData, sort_keys=True, indent=4, ensure_ascii=False)
-                yukino_1.sendMessage(msg.to, "グループ名の保護機能を解除しました。")
-            else:
-                yukino_1.sendMessage(msg.to, "グループ名の保護機能はすでに無効になっています。")
+        if msg.to in protect['groupName']:
+            del protect['groupName'][msg.to]
+            readData = codecs.open('./groupName.json','w','utf-8')
+            json.dump(protect['groupName'], readData, sort_keys=True, indent=4, ensure_ascii=False)
+            cl.replyMessage(msg, "グループ名の保護機能を解除しました。")
+        else:
+            cl.replyMessage(msg, "グループ名の保護機能はすでに無効になっています。")
         
     @tracer.Command(permissions=["Admin","White"])
     def 保護_画像_オン(self,cl,msg):
-        if msg._from in protect['whiteUsers']:
-            if msg.to in protect['groupPicture']:
-                yukino_1.sendMessage(msg.to, "グループ画像の保護機能はすでに有効になっています。")
-            else:
-                try:
-                    imageName = random_string(10) + ".png"
-                    download_img("http://dl.profile.line-cdn.net/"+yukino_1.getGroup(msg.to).pictureStatus, imageName)
-                    protect['groupPicture'][msg.to] = imageName
-                    readData = codecs.open('./groupPicture.json', 'w', 'utf-8')
-                    json.dump(protect['groupPicture'], readData, sort_keys=True, indent=4, ensure_ascii=False)
-                    yukino_1.sendMessage(msg.to, "グループ画像の保護機能を有効にしました。")
-                except:
-                    yukino_1.sendMessage(msg.to, "グループ画像が設定されていません。\n設定してからこのコマンドを実行してください。")
-        elif msg._from in admins:
-            if msg.to in protect['groupPicture']:
-                yukino_1.sendMessage(msg.to, "グループ画像の保護機能はすでに有効になっています。")
-            else:
-                try:
-                    imageName = random_string(10) + ".png"
-                    download_img("http://dl.profile.line-cdn.net/"+yukino_1.getGroup(msg.to).pictureStatus, imageName)
-                    protect['groupPicture'][msg.to] = imageName
-                    readData = codecs.open('./groupPicture.json', 'w', 'utf-8')
-                    json.dump(protect['groupPicture'], readData, sort_keys=True, indent=4, ensure_ascii=False)
-                    yukino_1.sendMessage(msg.to, "グループ画像の保護機能を有効にしました。")
-                except:
-                    yukino_1.sendMessage(msg.to, "グループ画像が設定されていません。\n設定してからこのコマンドを実行してください。")
+        if msg.to in protect['groupPicture']:
+            cl.replyMessage(msg, "グループ画像の保護機能はすでに有効になっています。")
+        else:
+            try:
+                imageName = random_string(10) + ".png"
+                download_img("http://dl.profile.line-cdn.net/"+cl.getGroup(msg.to).pictureStatus, imageName)
+                protect['groupPicture'][msg.to] = imageName
+                readData = codecs.open('./groupPicture.json', 'w', 'utf-8')
+                json.dump(protect['groupPicture'], readData, sort_keys=True, indent=4, ensure_ascii=False)
+                cl.replyMessage(msg, "グループ画像の保護機能を有効にしました。")
+            except:
+                cl.replyMessage(msg, "グループ画像が設定されていません。\n設定してからこのコマンドを実行してください。")
         
     @tracer.Command(permissions=["Admin","White"])
     def 保護_画像_オフ(self,cl,msg):
-        if msg._from in protect['whiteUsers']:
-            if msg.to in protect['groupPicture']:
-                imagePath = "./" + protect['groupPicture'][msg.to]
-                os.remove(imagePath)
-                del protect['groupPicture'][msg.to]
-                readData = codecs.open('./groupPicture.json','w','utf-8')
-                json.dump(protect['groupPicture'], readData, sort_keys=True, indent=4, ensure_ascii=False)
-                yukino_1.sendMessage(msg.to, "グループ画像の保護機能を解除しました。")
-            else:
-                yukino_1.sendMessage(msg.to, "グループ名の保護機能はすでに無効になっています。")
-        elif msg._from in admins:
-            if msg.to in protect['groupPicture']:
-                imagePath = "./" + protect['groupPicture'][msg.to]
-                os.remove(imagePath)
-                del protect['groupPicture'][msg.to]
-                readData = codecs.open('./groupPicture.json','w','utf-8')
-                json.dump(protect['groupPicture'], readData, sort_keys=True, indent=4, ensure_ascii=False)
-                yukino_1.sendMessage(msg.to, "グループ画像の保護機能を解除しました。")
-            else:
-                yukino_1.sendMessage(msg.to, "グループ名の保護機能はすでに無効になっています。")
+        if msg.to in protect['groupPicture']:
+            imagePath = "./" + protect['groupPicture'][msg.to]
+            os.remove(imagePath)
+            del protect['groupPicture'][msg.to]
+            readData = codecs.open('./groupPicture.json','w','utf-8')
+            json.dump(protect['groupPicture'], readData, sort_keys=True, indent=4, ensure_ascii=False)
+            cl.replyMessage(msg, "グループ画像の保護機能を解除しました。")
+        else:
+            cl.replyMessage(msg, "グループ名の保護機能はすでに無効になっています。")
         
     @tracer.Command(permissions=["Admin","White"])
     def 保護_url_オン(self,cl,msg):
-        if msg._from in protect['whiteUsers']:
-            if msg.to in protect['groupUrl']:
-                yukino_1.sendMessage(msg.to, "グループurlの保護機能はすでに有効になっています。")
-            else:
-                protect['groupUrl'][msg.to] = True
-                readData = codecs.open('./groupUrl.json', 'w', 'utf-8')
-                json.dump(protect['groupUrl'], readData, sort_keys=True, indent=4, ensure_ascii=False)
-                yukino_1.sendMessage(msg.to, "グループurlの保護機能を有効にしました。")
-                G =yukino_1.getGroup(msg.to)
-                G.preventJoinByTicket = True
-                yukino_1.updateGroup(G)
-        elif msg._from in admins:
-            if msg.to in protect['groupUrl']:
-                yukino_1.sendMessage(msg.to, "グループurlの保護機能はすでに有効になっています。")
-            else:
-                protect['groupUrl'][msg.to] = True
-                readData = codecs.open('./groupUrl.json', 'w', 'utf-8')
-                json.dump(protect['groupUrl'], readData, sort_keys=True, indent=4, ensure_ascii=False)
-                yukino_1.sendMessage(msg.to, "グループurlの保護機能を有効にしました。")
-                G =yukino_1.getGroup(msg.to)
-                G.preventJoinByTicket = True
-                yukino_1.updateGroup(G)
+        if msg.to in protect['groupUrl']:
+            cl.replyMessage(msg, "グループurlの保護機能はすでに有効になっています。")
+        else:
+            protect['groupUrl'][msg.to] = True
+            readData = codecs.open('./groupUrl.json', 'w', 'utf-8')
+            json.dump(protect['groupUrl'], readData, sort_keys=True, indent=4, ensure_ascii=False)
+            cl.replyMessage(msg, "グループurlの保護機能を有効にしました。")
+            G =cl.getGroup(msg.to)
+            G.preventJoinByTicket = True
+            cl.updateGroup(G)
         
     @tracer.Command(permissions=["Admin","White"])
     def 保護_url_オフ(self,cl,msg):
-        if msg._from in protect['whiteUsers']:
-            if msg.to in protect['groupUrl']:
-                del protect['groupUrl'][msg.to]
-                readData = codecs.open('./groupUrl.json','w','utf-8')
-                json.dump(protect['groupUrl'], readData, sort_keys=True, indent=4, ensure_ascii=False)
-                yukino_1.sendMessage(msg.to, "グループurlの保護機能を解除しました。")
-            else:
-                yukino_1.sendMessage(msg.to, "グループurlの保護機能はすでに無効になっています。")
-        elif msg._from in admins:
-            if msg.to in protect['groupUrl']:
-                del protect['groupUrl'][msg.to]
-                readData = codecs.open('./groupUrl.json','w','utf-8')
-                json.dump(protect['groupUrl'], readData, sort_keys=True, indent=4, ensure_ascii=False)
-                yukino_1.sendMessage(msg.to, "グループurlの保護機能を解除しました。")
-            else:
-                yukino_1.sendMessage(msg.to, "グループurlの保護機能はすでに無効になっています。")
+        if msg.to in protect['groupUrl']:
+            del protect['groupUrl'][msg.to]
+            readData = codecs.open('./groupUrl.json','w','utf-8')
+            json.dump(protect['groupUrl'], readData, sort_keys=True, indent=4, ensure_ascii=False)
+            cl.replyMessage(msg, "グループurlの保護機能を解除しました。")
+        else:
+            cl.replyMessage(msg, "グループurlの保護機能はすでに無効になっています。")
         
     @tracer.Command(permissions=["Admin","White"])
     def 招待URL許可(self,cl,msg):
         group = client.getGroup(msg.to)
         if group.preventedJoinByTicket == False:
-            yukino_1.sendMessage(msg.to, "既に許可されています。")
+            cl.replyMessage(msg, "既に許可されています。")
         else:
             if msg.to in protect['groupUrl']:
-                yukino_1.sendMessage(msg.to, "招待URLの設定変更が禁止されているので作成できません。\n保護 URL オフを実行してください。")
+                cl.replyMessage(msg, "招待URLの設定変更が禁止されているので作成できません。\n保護 URL オフを実行してください。")
             else:
                 group.preventedJoinByTicket = False
                 client.updateGroup(group)
-                yukino_1.sendMessage(msg.to, "URL招待を許可しました。")
+                cl.replyMessage(msg, "URL招待を許可しました。")
         
     @tracer.Command(permissions=["Admin","White"])
     def 招待URL拒否(self,cl,msg):
-        group = yukino_1.getGroup(msg.to)
+        group = cl.getGroup(msg.to)
         if group.preventedJoinByTicket == True:
-            yukino_1.sendMessage(msg.to, "既に拒否されています")
+            cl.replyMessage(msg, "既に拒否されています")
         else:
             group.preventedJoinByTicket = True
-            yukino_1.updateGroup(group)
-            yukino_1.sendMessage(msg.to, "URL招待を拒否しました")
+            cl.updateGroup(group)
+            cl.replyMessage(msg, "URL招待を拒否しました")
 
 tracer.addClass(FuncPs())
 tracer.addClass(OpPs())
